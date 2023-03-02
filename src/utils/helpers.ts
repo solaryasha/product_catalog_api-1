b2d054bef2e86eb8a33b3badb62b6621e2ba2882
Vladimir Arkanov <vladimirarkanov@icloud.com>
2023-03-02 13:52:44 +0200

    pagination and filter was done

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,6 +1,6 @@
-import { Phone } from '../server';
+import {Phone} from '../server';
 
-export const toSortData = (dataFromJson: Phone[], sortBy: string | unknown ) => {
+export const toSortData = (dataFromJson: Phone[], sortBy: string | unknown) => {
   if (sortBy === 'fromNewest') {
     dataFromJson = dataFromJson.sort((a, b) => b.year - a.year);
   }
@@ -14,26 +14,6 @@ export const toSortData = (dataFromJson: Phone[], sortBy: string | unknown ) => 
 
   if (sortBy === 'fromLowPrice') {
     dataFromJson = dataFromJson.sort((a, b) => a.fullPrice - b.fullPrice);
-  }
-
-  return dataFromJson;
-};
-
-export const phoneQuantity = (dataFromJson: Phone[], filterBy: string | unknown) => {
-  if (filterBy === '16') {
-    dataFromJson.length = 16;
-  }
-
-  if (filterBy === '20') {
-    dataFromJson.length = 20;
-  }
-
-  if (filterBy === '24') {
-    dataFromJson.length = 24;
-  }
-
-  if (filterBy === '28') {
-    dataFromJson.length = 28;
   }
 
   return dataFromJson;
